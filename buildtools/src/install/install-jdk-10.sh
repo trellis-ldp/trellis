088d3e921dabb2623b32a0c30a810f54576f6804
Aaron Coburn <acoburn@apache.org>
2018-03-20 13:42:46 -0400

    Update JDK 10 download URL

--- buildtools/src/install/install-jdk-10.sh
+++ buildtools/src/install/install-jdk-10.sh
@@ -5,14 +5,10 @@
 set -e
 
 JDK_FEATURE=10
-TMP=$(curl -L jdk.java.net/${JDK_FEATURE})              # extract most recent "JDK_BUILD" number from web page source
-TMP="${TMP#*Most recent build: jdk-${JDK_FEATURE}+}"    # remove everything before the number
-TMP="${TMP%%<*}"                                        # remove everything after the number
-JDK_BUILD="$(echo -e "${TMP}" | tr -d '[:space:]')"     # remove all whitespace
-JDK_ARCHIVE=openjdk-${JDK_FEATURE}+${JDK_BUILD}_linux-x64_bin.tar.gz
+JDK_ARCHIVE=openjdk-${JDK_FEATURE}_linux-x64_bin.tar.gz
 
 cd ~
-wget https://download.java.net/java/jdk${JDK_FEATURE}/archive/${JDK_BUILD}/GPL/${JDK_ARCHIVE}
+wget https://download.java.net/java/GA/jdk{$JDK_FEATURE}/{$JDK_FEATURE}/binaries/${JDK_ARCHIVE}
 tar -xzf ${JDK_ARCHIVE}
 export JAVA_HOME=~/jdk-${JDK_FEATURE}
 export PATH=${JAVA_HOME}/bin:$PATH
