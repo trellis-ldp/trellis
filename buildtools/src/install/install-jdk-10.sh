28a4ae4ffdb6a9190d69a265df8ccf35870e7848
Aaron Coburn <acoburn@apache.org>
2018-03-20 13:46:35 -0400

    Fix variable interpolation

--- buildtools/src/install/install-jdk-10.sh
+++ buildtools/src/install/install-jdk-10.sh
@@ -8,7 +8,7 @@ JDK_FEATURE=10
 JDK_ARCHIVE=openjdk-${JDK_FEATURE}_linux-x64_bin.tar.gz
 
 cd ~
-wget https://download.java.net/java/GA/jdk{$JDK_FEATURE}/{$JDK_FEATURE}/binaries/${JDK_ARCHIVE}
+wget https://download.java.net/java/GA/jdk${JDK_FEATURE}/${JDK_FEATURE}/binaries/${JDK_ARCHIVE}
 tar -xzf ${JDK_ARCHIVE}
 export JAVA_HOME=~/jdk-${JDK_FEATURE}
 export PATH=${JAVA_HOME}/bin:$PATH
