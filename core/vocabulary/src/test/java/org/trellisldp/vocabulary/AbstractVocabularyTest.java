17c7c25258c8b5f5b5092c294cfa41b70a877ea5
Aaron Coburn <acoburn@apache.org>
2018-05-04 14:11:51 -0400

    Turn down console warnings
    Related to #123

--- core/vocabulary/src/test/java/org/trellisldp/vocabulary/AbstractVocabularyTest.java
+++ core/vocabulary/src/test/java/org/trellisldp/vocabulary/AbstractVocabularyTest.java
@@ -76,7 +76,7 @@ public abstract class AbstractVocabularyTest {
                 assertTrue(subjects.contains(namespace() + field),
                         "Field definition is not in published ontology! " + field);
             } else if (!subjects.contains(namespace() + field)) {
-                LOGGER.warn("Field definition is not in published ontology! {}", field);
+                LOGGER.debug("Field definition is not in published ontology! {}", field);
             }
         });
     }
@@ -93,7 +93,7 @@ public abstract class AbstractVocabularyTest {
                 .filterKeep(Objects::nonNull)
                 .filterKeep(uri -> uri.startsWith(namespace())).filterDrop(namespace()::equals)
                 .filterDrop(subjects::contains).forEachRemaining(uri -> {
-            LOGGER.warn("{} not defined in {} class", uri, vocabulary().getName());
+            LOGGER.debug("{} not defined in {} class", uri, vocabulary().getName());
         });
     }
 
