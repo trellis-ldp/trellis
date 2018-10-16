fa0905cf2bad1e35dfee49fbbba292e1765e7647
ajs6f <ajs6f@apache.org>
2018-10-16 11:05:32 -0400

    Add Javadoc that initialization matters

--- core/api/src/main/java/org/trellisldp/api/ResourceService.java
+++ core/api/src/main/java/org/trellisldp/api/ResourceService.java
@@ -21,13 +21,18 @@ import static org.trellisldp.api.RDFUtils.getInstance;
 import java.util.Optional;
 import java.util.Set;
 
+import javax.annotation.PostConstruct;
+
 import org.apache.commons.rdf.api.BlankNode;
 import org.apache.commons.rdf.api.IRI;
 import org.apache.commons.rdf.api.RDFTerm;
 
 /**
- * The ResourceService provides methods for creating, retrieving and manipulating
- * Trellis resources.
+ * The ResourceService provides methods for creating, retrieving and manipulating Trellis resources.
+ * 
+ * Implementations should take care to provide any initialization needed, in a constructor or methods managed by an
+ * external framework (e.g. using {@link PostConstruct}). This may or may not include actions in external systems like
+ * databases, which may be better managed elsewhere.
  *
  * @author acoburn
  */
