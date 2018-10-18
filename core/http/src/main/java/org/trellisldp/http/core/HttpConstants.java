2be78acc12e43cdeb9d33bad2a8960dca33fc938
Aaron Coburn <acoburn@apache.org>
2018-10-18 10:20:23 -0400

    Add documentation to constant fields

--- core/http/src/main/java/org/trellisldp/http/core/HttpConstants.java
+++ core/http/src/main/java/org/trellisldp/http/core/HttpConstants.java
@@ -31,16 +31,22 @@ import org.apache.commons.rdf.api.IRI;
  */
 public final class HttpConstants {
 
+    /** The name of the HTTP request header used for Memento datetime negotiation. **/
     public static final String ACCEPT_DATETIME = "Accept-Datetime";
 
+    /** The name of the HTTP response header used to indicate the acceptable formats for PATCH requests. **/
     public static final String ACCEPT_PATCH = "Accept-Patch";
 
+    /** The name of the HTTP response header used to indicate the acceptable RDF formats in POST requests. **/
     public static final String ACCEPT_POST = "Accept-Post";
 
+    /** The name of the HTTP response header used to indicate the acceptable forms of range requests. **/
     public static final String ACCEPT_RANGES = "Accept-Ranges";
 
+    /** The ext parameter for ACL resources. **/
     public static final String ACL = "acl";
 
+    /** The default Memento TimeMap output format. **/
     public static final String APPLICATION_LINK_FORMAT = "application/link-format";
 
     /** Configuration key defining the server's base URL. **/
@@ -52,42 +58,61 @@ public final class HttpConstants {
     /** Configuration key defining whether to use weak ETags for RDF responses. **/
     public static final String CONFIG_HTTP_WEAK_ETAG = "trellis.http.weak.etag";
 
+    /** The Trellis query parameter for extended features of a given resource. **/
     public static final String EXT = "ext";
 
+    /** The Memento link parameter to indicate the datetime of a Memento. **/
     public static final String DATETIME = "datetime";
 
+    /** The Trellis ext parameter value used for accessing the description of an LDP-NR. **/
     public static final String DESCRIPTION = "description";
 
+    /** The name of the HTTP response header used to communicate instance digest values. **/
     public static final String DIGEST = "Digest";
 
+    /** The Memento link parameter indicating the beginning range of a TimeMap. **/
     public static final String FROM = "from";
 
+    /** The name of the HTTP response header used to indicate a link template to clients. **/
     public static final String LINK_TEMPLATE = "Link-Template";
 
+    /** The Memento link relation for mementos. **/
     public static final String MEMENTO = "memento";
 
+    /** The name of the HTTP response header used to indicate the date of a Memento resource. **/
     public static final String MEMENTO_DATETIME = "Memento-Datetime";
 
+    /** The Memento link relation for original resources. **/
     public static final String ORIGINAL = "original";
 
+    /** The name of the HTTP verb used to update resources. **/
     public static final String PATCH = "PATCH";
 
+    /** The name of the HTTP request header used to influence what information is included in responses. **/
     public static final String PREFER = "Prefer";
 
+    /** The name of the HTTP response header used to indicate how the Prefer header was applied. **/
     public static final String PREFERENCE_APPLIED = "Preference-Applied";
 
+    /** The name of the HTTP request header used to make range requests. **/
     public static final String RANGE = "Range";
 
+    /** The name of the Trellis session property. **/
     public static final String SESSION_PROPERTY = "session";
 
+    /** The Memento link relation for TimeGate resources. **/
     public static final String TIMEGATE = "timegate";
 
+    /** The Memento link relation for TimeMap resources. **/
     public static final String TIMEMAP = "timemap";
 
+    /** The name of the HTTP request header used to request an instance digest. **/
     public static final String WANT_DIGEST = "Want-Digest";
 
+    /** The Memento link parameter indicating the ending range of a TimeMap. **/
     public static final String UNTIL = "until";
 
+    /** The implied or default set of IRIs used with a Prefer header. **/
     public static final Set<String> DEFAULT_REPRESENTATION = unmodifiableSet(asList(PreferContainment, PreferMembership,
                 PreferUserManaged).stream().map(IRI::getIRIString).collect(toSet()));
 
