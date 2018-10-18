2ccabcd8ea4fe1f305f06a167df3762f682c2b7a
Aaron Coburn <acoburn@apache.org>
2018-10-18 10:34:13 -0400

    Replace strings with constants

--- core/http/src/main/java/org/trellisldp/http/TrellisHttpFilter.java
+++ core/http/src/main/java/org/trellisldp/http/TrellisHttpFilter.java
@@ -16,12 +16,22 @@ package org.trellisldp.http;
 import static java.util.Arrays.asList;
 import static java.util.Objects.isNull;
 import static java.util.Optional.ofNullable;
+import static javax.ws.rs.HttpMethod.DELETE;
+import static javax.ws.rs.HttpMethod.POST;
+import static javax.ws.rs.HttpMethod.PUT;
 import static javax.ws.rs.Priorities.AUTHORIZATION;
+import static javax.ws.rs.core.HttpHeaders.LINK;
 import static javax.ws.rs.core.Response.Status.BAD_REQUEST;
 import static javax.ws.rs.core.Response.Status.METHOD_NOT_ALLOWED;
 import static javax.ws.rs.core.Response.seeOther;
 import static javax.ws.rs.core.Response.status;
 import static javax.ws.rs.core.UriBuilder.fromUri;
+import static org.trellisldp.http.core.HttpConstants.ACCEPT_DATETIME;
+import static org.trellisldp.http.core.HttpConstants.DIGEST;
+import static org.trellisldp.http.core.HttpConstants.EXT;
+import static org.trellisldp.http.core.HttpConstants.PATCH;
+import static org.trellisldp.http.core.HttpConstants.PREFER;
+import static org.trellisldp.http.core.HttpConstants.RANGE;
 import static org.trellisldp.http.core.HttpConstants.TIMEMAP;
 
 import java.io.IOException;
@@ -45,7 +55,7 @@ import org.trellisldp.http.core.Version;
 @Priority(AUTHORIZATION - 20)
 public class TrellisHttpFilter implements ContainerRequestFilter {
 
-    private static final List<String> MUTATING_METHODS = asList("POST", "PUT", "DELETE", "PATCH");
+    private static final List<String> MUTATING_METHODS = asList(POST, PUT, DELETE, PATCH);
 
     @Override
     public void filter(final ContainerRequestContext ctx) throws IOException {
@@ -57,19 +67,19 @@ public class TrellisHttpFilter implements ContainerRequestFilter {
         }
 
         // Validate header/query parameters
-        ofNullable(ctx.getHeaderString("Accept-Datetime")).filter(x -> isNull(AcceptDatetime.valueOf(x)))
+        ofNullable(ctx.getHeaderString(ACCEPT_DATETIME)).filter(x -> isNull(AcceptDatetime.valueOf(x)))
             .ifPresent(x -> ctx.abortWith(status(BAD_REQUEST).build()));
 
         ofNullable(ctx.getHeaderString("Slug")).filter(s -> s.contains(slash)).ifPresent(x ->
             ctx.abortWith(status(BAD_REQUEST).build()));
 
-        ofNullable(ctx.getHeaderString("Prefer")).filter(x -> isNull(Prefer.valueOf(x))).ifPresent(x ->
+        ofNullable(ctx.getHeaderString(PREFER)).filter(x -> isNull(Prefer.valueOf(x))).ifPresent(x ->
             ctx.abortWith(status(BAD_REQUEST).build()));
 
-        ofNullable(ctx.getHeaderString("Range")).filter(x -> isNull(Range.valueOf(x))).ifPresent(x ->
+        ofNullable(ctx.getHeaderString(RANGE)).filter(x -> isNull(Range.valueOf(x))).ifPresent(x ->
             ctx.abortWith(status(BAD_REQUEST).build()));
 
-        ofNullable(ctx.getHeaderString("Link")).ifPresent(x -> {
+        ofNullable(ctx.getHeaderString(LINK)).ifPresent(x -> {
             try {
                 Link.valueOf(x);
             } catch (final IllegalArgumentException ex) {
@@ -77,7 +87,7 @@ public class TrellisHttpFilter implements ContainerRequestFilter {
             }
         });
 
-        ofNullable(ctx.getHeaderString("Digest")).filter(x -> isNull(Digest.valueOf(x))).ifPresent(x ->
+        ofNullable(ctx.getHeaderString(DIGEST)).filter(x -> isNull(Digest.valueOf(x))).ifPresent(x ->
             ctx.abortWith(status(BAD_REQUEST).build()));
 
         ofNullable(ctx.getUriInfo().getQueryParameters().getFirst("version")).ifPresent(x -> {
@@ -91,7 +101,7 @@ public class TrellisHttpFilter implements ContainerRequestFilter {
         });
 
         // Do not allow direct manipulation of timemaps
-        ofNullable(ctx.getUriInfo().getQueryParameters().get("ext")).filter(l -> l.contains(TIMEMAP))
+        ofNullable(ctx.getUriInfo().getQueryParameters().get(EXT)).filter(l -> l.contains(TIMEMAP))
             .filter(x -> MUTATING_METHODS.contains(ctx.getMethod()))
             .ifPresent(x -> ctx.abortWith(status(METHOD_NOT_ALLOWED).build()));
     }
