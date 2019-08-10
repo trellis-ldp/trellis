a6523d751ce192abcf7910f7d033333094a29a00
Aaron Coburn <acoburn@apache.org>
2019-08-10 07:42:46 -0400

    Microprofile deployment option
    Resolves #209

---
+++ platform/microprofile/src/main/java/org/trellisldp/microprofile/TrellisServiceSupplier.java
@@ -0,0 +1,54 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.trellisldp.microprofile;
+
+import static org.eclipse.microprofile.config.ConfigProvider.getConfig;
+import static org.trellisldp.triplestore.TriplestoreResourceService.CONFIG_TRIPLESTORE_RDF_LOCATION;
+import static org.trellisldp.triplestore.TriplestoreResourceService.buildRDFConnection;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.inject.Produces;
+
+import org.apache.jena.rdfconnection.RDFConnection;
+import org.trellisldp.api.EventService;
+import org.trellisldp.api.MementoService;
+import org.trellisldp.api.NoopEventService;
+import org.trellisldp.file.FileMementoService;
+
+/**
+ * A managed bean that generates an RDF connection for the
+ * triplestore resource service.
+ */
+@ApplicationScoped
+public class TrellisServiceSupplier {
+
+    @Produces
+    private RDFConnection rdfConnection;
+
+    @Produces
+    private MementoService mementoService;
+
+    @Produces
+    private EventService eventService;
+
+    @PostConstruct
+    private void init() {
+        rdfConnection = buildRDFConnection(getConfig().getOptionalValue(CONFIG_TRIPLESTORE_RDF_LOCATION, String.class)
+            .orElse(null));
+
+        mementoService = new FileMementoService();
+        eventService = new NoopEventService();
+    }
+}
---
+++ platform/microprofile/src/test/java/org/trellisldp/microprofile/SmokeIT.java
@@ -0,0 +1,49 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.trellisldp.microprofile;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.slf4j.LoggerFactory.getLogger;
+
+import java.io.IOException;
+
+import org.apache.http.Header;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClients;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+
+class SmokeIT {
+
+    private static final Logger LOGGER = getLogger(SmokeIT.class);
+
+    private static final String testUri = "http://localhost:" + Integer.getInteger("testServerHttpPort", 9080) + "/";
+
+    private static final HttpClient testClient = HttpClients.createDefault();
+
+    @Test
+    void findTrellis() throws IOException {
+        final HttpResponse response = testClient.execute(new HttpGet(testUri));
+        final int statusCode = response.getStatusLine().getStatusCode();
+        final String statusPhrase = response.getStatusLine().getReasonPhrase();
+        LOGGER.info("Got response {} for {}", statusCode, statusPhrase);
+        for (final Header h : response.getAllHeaders()) {
+            LOGGER.info("Header: {} {}", h.getName(), h.getValue());
+        }
+        assertEquals(200, statusCode);
+    }
+
+}
