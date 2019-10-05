4f43505f728daabdc19642238065fae5e052e87a
Aaron Coburn <acoburn@apache.org>
2019-10-04 20:24:43 -0400

    Rename openliberty project

--- platform/openliberty/src/main/java/org/trellisldp/openliberty/TrellisServiceSupplier.java
+++ platform/openliberty/src/main/java/org/trellisldp/openliberty/TrellisServiceSupplier.java
@@ -11,7 +11,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.trellisldp.app.mp.triplestore;
+package org.trellisldp.openliberty;
 
 import static org.eclipse.microprofile.config.ConfigProvider.getConfig;
 import static org.trellisldp.triplestore.TriplestoreResourceService.CONFIG_TRIPLESTORE_RDF_LOCATION;
