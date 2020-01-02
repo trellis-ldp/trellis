33428b6eabd98b68cb333857fcf4acb604bd02f6
Aaron Coburn <acoburn@apache.org>
2020-01-02 13:30:51 -0500

    Adjust gh package rules
    Publish GitHub packages on releases only

--- buildtools/src/main/resources/docker/publishToGitHub.sh
+++ buildtools/src/main/resources/docker/publishToGitHub.sh
@@ -3,22 +3,13 @@
 IMAGE=docker.pkg.github.com/trellis-ldp/trellis/trellis-triplestore
 
 VERSION=$(./gradlew -q getVersion)
-BRANCH=$(git branch 2>/dev/null | sed -n -e 's/^\* \(.*\)/\1/p')
 
-cd platform/quarkus
-../../gradlew assemble
+# Publish releases only
+if [[ $VERSION != *SNAPSHOT* ]]; then
+    cd platform/quarkus
+    ../../gradlew assemble
 
-TAG=latest
-# Use the develop tag for snapshots
-if [[ $VERSION == *SNAPSHOT* ]]; then
-    TAG=develop
-fi
-
-# Don't use latest/develop tags for maintenance branches
-if [[ $BRANCH == *.x ]]; then
     docker build -f src/main/docker/Dockerfile.jvm -t "$IMAGE:$VERSION" .
-else
-    docker build -f src/main/docker/Dockerfile.jvm -t "$IMAGE:$TAG" -t "$IMAGE:$VERSION" .
+    docker push "$IMAGE:$VERSION"
 fi
 
-docker push $IMAGE
