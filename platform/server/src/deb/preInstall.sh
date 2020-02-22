5a3475e75f57d6dfbf6d159c249df6223effe3f1
Aaron Coburn <acoburn@apache.org>
2020-02-21 20:42:33 -0500

    Remove ospackage plugin
    Resolves #693

--- platform/server/src/deb/preInstall.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-adduser --system --home /opt/trellis --group trellis
--- platform/server/src/deb/preUninstall.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-systemctl stop trellis
-systemctl disable trellis
-systemctl daemon-reload
-systemctl reset-failed
