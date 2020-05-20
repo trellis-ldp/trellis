32e895dfadcad419dfe912b2af68e9d4e50f9c20
Aaron Coburn <acoburn@apache.org>
2020-05-20 13:58:46 -0400

    Add JDBC backend for Quarkus application

--- buildtools/src/main/resources/docker/publishToDockerHub.sh
+++ buildtools/src/main/resources/docker/publishToDockerHub.sh
@@ -1,12 +1,37 @@
 #!/bin/bash
 
-# Quarkus-based image
-IMAGE=trellisldp/trellis-triplestore
-
 VERSION=$(./gradlew -q getVersion)
 BRANCH=$(git branch 2>/dev/null | sed -n -e 's/^\* \(.*\)/\1/p')
-
 cd platform/quarkus
+
+##################################
+# Quarkus-based triplestore image
+##################################
+IMAGE=trellisldp/trellis-triplestore
+
+../../gradlew assemble -Ptriplestore
+
+TAG=latest
+# Use the develop tag for snapshots
+if [[ $VERSION == *SNAPSHOT* ]]; then
+    TAG=develop
+fi
+
+# Don't use latest/develop tags for maintenance branches
+if [[ $BRANCH == *.x ]]; then
+    docker build -f src/main/docker/Dockerfile.jvm -t "$IMAGE:$VERSION" .
+else
+    docker build -f src/main/docker/Dockerfile.jvm -t "$IMAGE:$TAG" -t "$IMAGE:$VERSION" .
+fi
+
+docker push $IMAGE
+
+
+###############################
+# Quarkus-based database image
+###############################
+IMAGE=trellisldp/trellis-database
+
 ../../gradlew assemble
 
 TAG=latest
@@ -24,7 +49,10 @@ fi
 
 docker push $IMAGE
 
+
+#########################
 # Dropwizard-based image
+#########################
 IMAGE=trellisldp/trellis
 
 cd ../dropwizard
--- buildtools/src/main/resources/docker/publishToGitHub.sh
+++ buildtools/src/main/resources/docker/publishToGitHub.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
 
-IMAGE=docker.pkg.github.com/trellis-ldp/trellis/trellis-triplestore
-
 VERSION=$(./gradlew -q getVersion)
+cd platform/quarkus
+
+##################################
+# Quarkus-based triplestore image
+##################################
+IMAGE=docker.pkg.github.com/trellis-ldp/trellis/trellis-triplestore
 
 # Publish releases only
 if [[ $VERSION != *SNAPSHOT* ]]; then
-    cd platform/quarkus
+    ../../gradlew assemble -Ptriplestore
+
+    docker build -f src/main/docker/Dockerfile.jvm -t "$IMAGE:$VERSION" .
+    docker push "$IMAGE:$VERSION"
+fi
+
+###############################
+# Quarkus-based database image
+###############################
+IMAGE=docker.pkg.github.com/trellis-ldp/trellis/trellis-database
+
+# Publish releases only
+if [[ $VERSION != *SNAPSHOT* ]]; then
     ../../gradlew assemble
 
     docker build -f src/main/docker/Dockerfile.jvm -t "$IMAGE:$VERSION" .
