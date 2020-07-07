7ff22e644bb0b0b79fd7b9a6a744cd60b5ef6567
Aaron Coburn <acoburn@apache.org>
2020-07-06 20:34:33 -0400

    Change name of non-RDF table (#952)

--- components/jdbc/src/main/resources/db/migration/V0.6__Trellis.sql
+++ components/jdbc/src/main/resources/db/migration/V0.6__Trellis.sql
@@ -2,13 +2,13 @@
 -- binary TABLE
 --
 
-CREATE TABLE public.binary (
+CREATE TABLE public.nonrdf (
     id character varying(1024) PRIMARY KEY,
     data bytea NOT NULL
 );
 
-COMMENT ON TABLE public.binary IS 'This table can be used to store binary data for a resource.';
+COMMENT ON TABLE public.nonrdf IS 'This table can be used to store non-RDF data for a resource.';
 
-COMMENT ON COLUMN public.binary.id IS 'This value is the unique identifier for the binary.';
-COMMENT ON COLUMN public.binary.data IS 'This holds the binary data itself, limited to 1GB in size';
+COMMENT ON COLUMN public.nonrdf.id IS 'This value is the unique identifier for the non-RDF resource.';
+COMMENT ON COLUMN public.nonrdf.data IS 'This holds the non-RDF data itself, limited to 1GB in size';
 
