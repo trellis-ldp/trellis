9d832ced002f09027e972aa9ec152e7bee40bcf1
Aaron Coburn <acoburn@apache.org>
2021-01-27 10:11:33 -0500

    CDI injection for notification services

--- notifications/jms/src/main/java/org/trellisldp/jms/JmsEventService.java
+++ notifications/jms/src/main/java/org/trellisldp/jms/JmsEventService.java
@@ -16,22 +16,18 @@
 package org.trellisldp.jms;
 
 import static java.util.Objects.requireNonNull;
-import static javax.jms.Session.AUTO_ACKNOWLEDGE;
-import static org.eclipse.microprofile.config.ConfigProvider.getConfig;
 import static org.slf4j.LoggerFactory.getLogger;
 
 import javax.inject.Inject;
-import javax.jms.Connection;
 import javax.jms.JMSException;
 import javax.jms.Message;
-import javax.jms.MessageProducer;
 import javax.jms.Session;
 
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.slf4j.Logger;
 import org.trellisldp.api.Event;
 import org.trellisldp.api.EventSerializationService;
 import org.trellisldp.api.EventService;
-import org.trellisldp.api.NoopEventSerializationService;
 
 /**
  * A JMS message producer capable of publishing messages to a JMS broker such as ActiveMQ.
@@ -43,80 +39,25 @@ public class JmsEventService implements EventService {
     /** The configuration key controlling the JMS queue name. */
     public static final String CONFIG_JMS_QUEUE_NAME = "trellis.jms.queue-name";
 
-    /** The configuration key controlling the JMS broker URL. */
-    public static final String CONFIG_JMS_URL = "trellis.jms.url";
-
-    /** The configuration key controlling the JMS username. */
-    public static final String CONFIG_JMS_USERNAME = "trellis.jms.username";
-
-    /** The configuration key controlling the JMS password. */
-    public static final String CONFIG_JMS_PASSWORD = "trellis.jms.password";
-
     /** The configuration key controlling whether to use a topic or queue. */
     public static final String CONFIG_JMS_USE_QUEUE = "trellis.jms.use-queue";
 
     private static final Logger LOGGER = getLogger(JmsEventService.class);
 
-    private final EventSerializationService serializer;
-    private final MessageProducer producer;
-    private final Session session;
-
-    /**
-     * Create a new JMS Publisher.
-     *
-     * @apiNote This construtor is used by CDI runtimes that require a public, no-argument constructor.
-     *          It should not be invoked directly in user code.
-     */
-    public JmsEventService() {
-        this.serializer = new NoopEventSerializationService();
-        this.session = null;
-        this.producer = null;
-    }
-
-    /**
-     * Create a new JMS Publisher.
-     * @param serializer the event serializer
-     * @param conn the connection
-     * @throws JMSException when there is a connection error
-     */
     @Inject
-    public JmsEventService(final EventSerializationService serializer, final Connection conn) throws JMSException {
-        this(serializer, conn.createSession(false, AUTO_ACKNOWLEDGE),
-                getConfig().getValue(CONFIG_JMS_QUEUE_NAME, String.class),
-                getConfig().getOptionalValue(CONFIG_JMS_USE_QUEUE, Boolean.class).orElse(Boolean.TRUE));
-    }
+    @ConfigProperty(name = CONFIG_JMS_USE_QUEUE,
+                    defaultValue = "true")
+    boolean useQueue;
 
-    /**
-     * Create a new JMS Publisher.
-     * @param serializer the event serializer
-     * @param session the JMS session
-     * @param queueName the name of the queue
-     * @throws JMSException when there is a connection error
-     */
-    public JmsEventService(final EventSerializationService serializer, final Session session, final String queueName)
-            throws JMSException {
-        this(serializer, session, queueName, true);
-    }
+    @Inject
+    @ConfigProperty(name = CONFIG_JMS_QUEUE_NAME)
+    String queueName;
 
-    /**
-     * Create a new JMS Publisher.
-     * @param serializer the event serializer
-     * @param session the JMS session
-     * @param queueName the name of the queue
-     * @param useQueue whether to use a queue or a topic
-     * @throws JMSException when there is a connection error
-     */
-    public JmsEventService(final EventSerializationService serializer, final Session session, final String queueName,
-            final boolean useQueue) throws JMSException {
-        requireNonNull(queueName, "JMS Queue name may not be null!");
-        this.serializer = requireNonNull(serializer, "Event serializer may not be null!");
-        this.session = requireNonNull(session, "JMS Session may not be null!");
-        if (useQueue) {
-            this.producer = session.createProducer(session.createQueue(queueName));
-        } else {
-            this.producer = session.createProducer(session.createTopic(queueName));
-        }
-    }
+    @Inject
+    EventSerializationService serializer;
+
+    @Inject
+    Session session;
 
     @Override
     public void emit(final Event event) {
@@ -125,7 +66,11 @@ public class JmsEventService implements EventService {
         try {
             final Message message = session.createTextMessage(serializer.serialize(event));
             message.setStringProperty("Content-Type", "application/ld+json");
-            producer.send(message);
+            if (useQueue) {
+                session.createProducer(session.createQueue(queueName)).send(message);
+            } else {
+                session.createProducer(session.createTopic(queueName)).send(message);
+            }
         } catch (final JMSException ex) {
             LOGGER.error("Error writing to broker: {}", ex.getMessage());
         }
--- notifications/kafka/src/main/java/org/trellisldp/kafka/KafkaEventService.java
+++ notifications/kafka/src/main/java/org/trellisldp/kafka/KafkaEventService.java
@@ -16,23 +16,18 @@
 package org.trellisldp.kafka;
 
 import static java.util.Objects.requireNonNull;
-import static org.eclipse.microprofile.config.ConfigProvider.getConfig;
 import static org.slf4j.LoggerFactory.getLogger;
-
-import java.util.Properties;
 
 import javax.inject.Inject;
 
 import org.apache.commons.rdf.api.IRI;
-import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
-import org.eclipse.microprofile.config.Config;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.slf4j.Logger;
 import org.trellisldp.api.Event;
 import org.trellisldp.api.EventSerializationService;
 import org.trellisldp.api.EventService;
-import org.trellisldp.api.NoopEventSerializationService;
 
 /**
  * A Kafka message producer capable of publishing messages to a Kafka cluster.
@@ -44,54 +39,15 @@ public class KafkaEventService implements EventService {
     /** The configuration key controlling the name of the kafka topic. */
     public static final String CONFIG_KAFKA_TOPIC = "trellis.kafka.topic";
 
-    private final EventSerializationService serializer;
-    private final Producer<String, String> producer;
-    private final String topic;
-
-    /**
-     * Create a new Kafka Event Service with a no-op serializer.
-     *
-     * @apiNote This construtor is used by CDI runtimes that require a public, no-argument constructor.
-     *          It should not be invoked directly in user code.
-     */
-    public KafkaEventService() {
-        this(new NoopEventSerializationService());
-    }
-
-    /**
-     * Create a new Kafka Event Service.
-     * @param serializer the event serializer
-     */
     @Inject
-    public KafkaEventService(final EventSerializationService serializer) {
-        this(serializer, getConfig());
-    }
+    EventSerializationService serializer;
 
-    private KafkaEventService(final EventSerializationService serializer, final Config config) {
-        this(serializer, buildProducer(config), config.getValue(CONFIG_KAFKA_TOPIC, String.class));
-    }
+    @Inject
+    Producer<String, String> producer;
 
-    /**
-     * Create a new Kafka Event Service.
-     * @param serializer the event serializer
-     * @param producer the producer
-     */
-    public KafkaEventService(final EventSerializationService serializer, final Producer<String, String> producer) {
-        this(serializer, producer, getConfig().getValue(CONFIG_KAFKA_TOPIC, String.class));
-    }
-
-    /**
-     * Create a new Kafka Event Service.
-     * @param serializer the event serializer
-     * @param producer the producer
-     * @param topic the name of the kafka topic
-     */
-    public KafkaEventService(final EventSerializationService serializer, final Producer<String, String> producer,
-            final String topic) {
-        this.serializer = requireNonNull(serializer, "The Event serializer may not be null!");
-        this.producer = requireNonNull(producer, "Kafka producer may not be null!");
-        this.topic = requireNonNull(topic, "Kafka topic name may not be null!");
-    }
+    @Inject
+    @ConfigProperty(name = CONFIG_KAFKA_TOPIC)
+    String topic;
 
     @Override
     public void emit(final Event event) {
@@ -100,27 +56,5 @@ public class KafkaEventService implements EventService {
         LOGGER.debug("Sending message to Kafka topic: {}", topic);
         producer.send(new ProducerRecord<>(topic, event.getObject().map(IRI::getIRIString).orElse(null),
                     serializer.serialize(event)));
-    }
-
-    private static Producer<String, String> buildProducer(final Config config) {
-        final String prefix = "trellis.kafka.";
-        final Properties p = new Properties();
-        p.setProperty("acks", config.getOptionalValue(prefix + "acks", String.class).orElse("all"));
-        p.setProperty("batch.size", config.getOptionalValue(prefix + "batch.size", String.class).orElse("16384"));
-        p.setProperty("retries", config.getOptionalValue(prefix + "retries", String.class).orElse("0"));
-        p.setProperty("linger.ms", config.getOptionalValue(prefix + "linger.ms", String.class).orElse("1"));
-        p.setProperty("buffer.memory", config.getOptionalValue(prefix + "buffer.memory", String.class)
-                .orElse("33554432"));
-        config.getPropertyNames().forEach(prop -> {
-            if (prop.startsWith(prefix) && !CONFIG_KAFKA_TOPIC.equals(prop)) {
-                p.setProperty(prop.substring(prefix.length()), config.getValue(prop, String.class));
-            }
-        });
-
-        p.setProperty("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-        p.setProperty("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-        p.setProperty("bootstrap.servers", config.getValue("trellis.kafka.bootstrap.servers", String.class));
-
-        return new KafkaProducer<>(p);
     }
 }
